b940ba35e8f17ae161ccc557bcb9c6309dddff9d
ChristianDuarteL <w.christian.duarte@outlook.com>
2023-08-14 10:28:38 -0600

    arc() y lineTo()

--- index.js
+++ index.js
@@ -11,4 +11,21 @@ window.addEventListener('load', () => {
     
     ctx.fillStyle = "rgba(255,0,255,0.5)";
     ctx.fillRect(260, 260, 100, 100);
+
+    ctx.fillStyle = "#ff8888";
+    ctx.strokeStyle = "#ff8888";
+    ctx.lineWidth = 4;
+    ctx.beginPath();
+    ctx.moveTo(300, 200)
+    ctx.lineTo(350, 200);
+    ctx.lineTo(325, 150);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(400, 110, 50, 0, 2 * Math.PI);
+    ctx.fill();
+    
+    ctx.beginPath();
+    ctx.arc(510, 110, 50, 0, 2 * Math.PI);
+    ctx.stroke();
 })
